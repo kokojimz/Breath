f8d74045d73508974e022d5c8cfafb25c8b9e85a
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-01-13 23:12:41 +0700

    first commit

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,7 +1,7 @@
 async function Page() {
     return (
         <main>
-            <h1 className="head-text">Onboarding</h1>
+            <h1 className="head-text">Onboardingg</h1>
         </main>
     )
 }
