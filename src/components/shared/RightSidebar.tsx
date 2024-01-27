d9aa7236e2327653f53f7c44e4fbd2d80f35b4dd
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-01-27 18:23:51 +0700

    feat: Manage Rightsidebar components

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,6 +1,17 @@
 function RightSidebar() {
     return (
-        <h1>ightSidebar</h1>
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex-1 flex flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Suggested Communities
+                </h3>
+            </div>
+            <div className="flex-1 flex flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">
+                    Suggested Users
+                </h3>
+            </div>
+        </section>
     )
 }
 
