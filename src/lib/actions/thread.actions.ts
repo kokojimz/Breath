51ad677fe60c9a5dd7583647925ecce44a8b1216
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-02-03 19:28:26 +0700

    feat: fetching posts to Homepage

--- src/lib/actions/thread.actions.ts
+++ src/lib/actions/thread.actions.ts
@@ -33,3 +33,42 @@ export async function createThread({ text, author, communityId, path }: Params) 
         throw new Error(`Failed to create thread: ${error.message}`);
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // Calculate the number of posts to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    //Fetch the posts that have no parents (top level threads)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: "desc" })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({
+            path: "author",
+            model: User,
+        })
+        // .populate({
+        //     path: "community",
+        //     model: Community,
+        // })
+        .populate({
+            path: "children", // Populate the children field
+            populate: {
+                path: "author", // Populate the author field within children
+                model: User,
+                select: "_id name parentId image", // Select only _id and username fields of the author
+            },
+        });
+
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] },
+    });
+    // Get the total count of posts
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+    return { posts, isNext };
+}
