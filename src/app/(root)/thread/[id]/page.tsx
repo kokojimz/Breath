9dd04479a85074ca0715461e01da361191506d1e
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-02-04 21:04:43 +0700

    feat: Add back-end functionality for add/reply comment

--- src/app/(root)/thread/[id]/page.tsx
+++ src/app/(root)/thread/[id]/page.tsx
@@ -5,6 +5,7 @@ import ThreadCard from "@/components/cards/ThreadCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
+import Comment from "@/components/forms/Comment";
 
 export const revalidate = 0;
 
@@ -34,6 +35,30 @@ async function page({ params }: { params: { id: string } }) {
                 />
             </div>
 
+            <div className='mt-7'>
+                <Comment
+                    threadId={params.id}
+                    currentUserImg={userInfo.image}
+                    currentUserId={JSON.stringify(userInfo._id)}
+                />
+            </div>
+
+            <div className='mt-10'>
+                {thread.children.map((childItem: any) => (
+                    <ThreadCard
+                        key={childItem._id}
+                        id={childItem._id}
+                        currentUserId={user.id}
+                        parentId={childItem.parentId}
+                        content={childItem.text}
+                        author={childItem.author}
+                        community={childItem.community}
+                        createdAt={childItem.createdAt}
+                        comments={childItem.children}
+                        isComment
+                    />
+                ))}
+            </div>
         </section>
     );
 }
