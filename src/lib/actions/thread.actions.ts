da066c2967f997822e256082abd0f75c2dd91583
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-02-04 09:46:08 +0700

    feat: Implementing multi-level comment functionality for Thread card structure

--- src/lib/actions/thread.actions.ts
+++ src/lib/actions/thread.actions.ts
@@ -72,3 +72,41 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     const isNext = totalPostsCount > skipAmount + posts.length;
     return { posts, isNext };
 }
+
+export async function fetchThreadById(threadId: string) {
+    connectToDB();
+
+    try {
+        const thread = await Thread.findById(threadId)
+            .populate({
+                path: "author",
+                model: User,
+                select: "_id id name image",
+            }) // TODO: Populate the community field with _id and name
+            .populate({
+                path: "children", // Populate the children field
+                populate: [
+                    {
+                        path: "author", // Populate the author field within children
+                        model: User,
+                        select: "_id id name parentId image", // Select only _id and username fields of the author
+                    },
+                    {
+                        path: "children", // Populate the children field within children
+                        model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+                        populate: {
+                            path: "author", // Populate the author field within nested children
+                            model: User,
+                            select: "_id id name parentId image", // Select only _id and username fields of the author
+                        },
+                    },
+                ],
+            })
+            .exec();
+
+        return thread;
+    } catch (err: any) {
+        console.error("Error while fetching thread:", err);
+        throw new Error("Unable to fetch thread");
+    }
+}
