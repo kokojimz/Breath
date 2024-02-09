7921a7271dec01e1fe6904f673a488a9b83d3fee
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-02-10 00:02:19 +0700

    fix: ignore ts errors

--- src/components/cards/UserCard.tsx
+++ src/components/cards/UserCard.tsx
@@ -16,8 +16,6 @@ interface Props {
 function UserCard({ id, name, username, imgUrl, personType }: Props) {
     const router = useRouter();
 
-    const isCommunity = personType === "Community";
-
     return (
         <article className='user-card'>
             <div className='user-card_avatar'>
@@ -38,13 +36,7 @@ function UserCard({ id, name, username, imgUrl, personType }: Props) {
 
             <Button
                 className='user-card_btn'
-                onClick={() => {
-                    if (isCommunity) {
-                        router.push(`/communities/${id}`);
-                    } else {
-                        router.push(`/profile/${id}`);
-                    }
-                }}
+                onClick={() => router.push(`/profile/${id}`)}
             >
                 View
             </Button>
