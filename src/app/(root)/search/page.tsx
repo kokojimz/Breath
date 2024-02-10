44d4167e9e832a1ab12bcd643def077f90c00a60
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2024-02-10 14:32:28 +0700

    feat: add CommunityCard  components

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -1,10 +1,17 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-async function Page() {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+async function Page({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) {
     const user = await currentUser();
     if (!user) return null;
 
@@ -13,14 +20,16 @@ async function Page() {
 
     const result = await fetchUsers({
         userId: user.id,
-        searchString: '',
-        pageNumber: 1,
+        searchString: searchParams.q,
+        pageNumber: searchParams?.page ? +searchParams.page : 1,
         pageSize: 25,
     });
 
     return (
         <section>
             <h1 className='head-text mb-10'>Search</h1>
+
+            <Searchbar routeType='search' />
 
             <div className='mt-14 flex flex-col gap-9'>
                 {result.users.length === 0 ? (
@@ -40,6 +49,12 @@ async function Page() {
                     </>
                 )}
             </div>
+
+            <Pagination
+                path='search'
+                pageNumber={searchParams?.page ? +searchParams.page : 1}
+                isNext={result.isNext}
+            />
         </section>
     );
 }
